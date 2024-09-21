69f6a6c5ca243e7c37103bb68afc7a5a7d7f376b
Marek Chadim <chadimarek@gmail.com>
2024-09-21 01:33:08 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27187,7 +27187,7 @@ var _stockholmJpgDefault = parcelHelpers.interopDefault(_stockholmJpg);
 const imageAltText = "Stockholm";
 /**
  * Short description that expands on your title on the Home component.
- */ const description = "I am an MSc Economics student focused on econometrics and applied microeconomics. I am interested in research that helps create and improve social policies for education, healthcare, and workforce issues.";
+ */ const description = "I am an MSc Economics student focused on econometrics and applied microeconomics.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
