9689b927e42ae522880440d489a24eae34001a6b
Marek Chadim <chadimarek@gmail.com>
2025-04-18 12:11:01 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27221,7 +27221,7 @@ var _stockholmJpgDefault = parcelHelpers.interopDefault(_stockholmJpg);
 const imageAltText = "Stockholm";
 /**
  * Short description that expands on your title on the Home component.
- */ const description = "Hi and thanks for visiting my academic portfolio website!";
+ */ const description = "Welcome to my Pre-Doctoral applications website!";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
